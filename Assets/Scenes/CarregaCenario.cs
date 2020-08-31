70fb5703c7c03f681182e27dfd384b9a8f408e93
antonio-pinheiro <antonio.fatecjd@gmail.com>
2020-08-31 18:53:43 -0300

    Primeiro commit

---
+++ Assets/Scenes/CarregaCenario.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregaCenario : MonoBehaviour
+{
+    public void Trocar()
+    {
+        SceneManager.LoadScene("interior_casa");
+    }
+}
---
+++ Assets/Scripts/SignalListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class SignalListener : MonoBehaviour
+{
+public Signal signal;
+public UnityEvent signalEvent;
+    
+
+    public void OnSignalRaised()
+    {
+        signalEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        signal.RegisterListener(this);
+
+
+    }
+
+    private void disable()
+    {
+        signal.DeRegisterListener(this);
+    }
+    
+}
